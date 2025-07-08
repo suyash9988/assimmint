de5eb1ecef187fae391d9879031df549776a4775
suyash9988 <thangesuyash7@gmail.com>
2025-07-08 10:25:36 +0530

    Add files via upload

---
+++ Untitled4.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    float num1,num2,num3,num4,num5,sum,average;
+    
+    printf("Enter five numbers:\n");
+    scanf("%f %f %f %f %f", &num1, &num2, &num3, &num4, &num5);
+    
+    sum = num1 + num2 + num3 + num4 + num5;
+    average = sum / 5;
+    
+    printf("Average of the five numbers is: %.2f\n", average);
+    
+    return 0;
+
+}
